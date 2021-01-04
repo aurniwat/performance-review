96fad9750dfc461bff12fa5e46bb47cfac7e90f4
aurniwat <james@sennalabs.com>
2021-01-04 18:10:56 +0700

    calculate mbti

--- mbti/findCognitiveFunction.js
+++ mbti/findCognitiveFunction.js
@@ -8,5 +8,28 @@
  * }}
  */
 export function findCognitiveFunction(mbti) {
-  throw new Error("Not implemented");
+  const mapFlipValue = {
+    I: "E",
+    S: "N",
+    T: "F",
+    J: "P",
+    E: "I",
+    N: "S",
+    F: "T",
+    P: "J"
+  }
+  const getFlipValue = value => {
+    return mapFlipValue[value]
+  }
+
+  const mbtiList = mbti.split("")
+  const fn1 = mbtiList[1]
+  const fn2 = mbtiList[2]
+
+  const dominant = `${fn1}${mbtiList[3] === "P" ? "e" : "i"}`
+  const auxiliary = `${fn2}${mbtiList[3] === "P" ? "i" : "e"}`
+  const tertiary = `${getFlipValue(auxiliary[0])}${auxiliary[1] === "e" ? "i" : "e"}`
+  const inferior = `${getFlipValue(dominant[0])}${dominant[1] === "e" ? "i" : "e"}`
+
+  return { dominant, auxiliary, tertiary, inferior }
 }
