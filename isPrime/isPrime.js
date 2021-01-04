c98c15fde6940fc957d029c2460ebd0fe866a1c1
aurniwat <james@sennalabs.com>
2021-01-04 17:08:24 +0700

    check num that more than 2 is it prime or not

--- isPrime/isPrime.js
+++ isPrime/isPrime.js
@@ -3,5 +3,18 @@ export function isPrime(num) {
     return false
   } else if (num === 2) {
     return true
+  } else if (num > 2) {
+    let isPrime = true
+    for (let i = 2; i <= num; i++) {
+      if (num / i === 0) {
+        isPrime = false
+        break
+      }
+    }
+    if (!isPrime) {
+      return false
+    } else {
+      return true
+    }
   }
 }
